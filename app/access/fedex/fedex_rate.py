31c904231bc1bda2d44d534bef47c061f598883f
Peter Gunarso <peter@gunar.so>
2019-09-20 14:54:56 -0700

    support for fedex ratres

--- app/access/fedex/fedex_rate.py
+++ app/access/fedex/fedex_rate.py
@@ -1,48 +1,101 @@
 import json
-from fedex.config import FedexConfig
-from fedex.services.rate_service import FedexRateServiceRequest
-from fedex.tools.conversion import sobject_to_dict
+import re
+import xml.etree.ElementTree as ET
+from zeep import Client, Settings
+from zeep.exceptions import Fault, TransportError, XMLSyntaxError
 
-with open("app/access/creds/creds.json") as f:
+import pandas as pd
+from zeep.helpers import serialize_object
+
+settings = Settings(strict=False, xml_huge_tree=False)
+client = Client('RateService_v26.wsdl', settings=settings)
+
+with open("../creds/creds.json", "r") as f:
     credentials = json.load(f)['fedex']
 
-CONFIG_OBJ = FedexConfig(key=credentials['api_key'],
-                         password=credentials['password'],
-                         account_number=credentials['ship_num'],
-                         use_test_server=True)
+RateRequestDict = {
+    "WebAuthenticationDetail": {
+        "UserCredential": {
+            "Key": credentials['api_key'],
+            "Password": credentials['password']
+        }
+    },
+    "ClientDetail": {
+        "AccountNumber": credentials['ship_num'],
+        "MeterNumber": credentials['meter_num']
+    },
+    "Version": {
+        "ServiceId": "crs",
+        "Major": 26,
+        "Intermediate": 0,
+        "Minor": 0
+    },
+    "RequestedShipment": {
+        "DropoffType": "REGULAR_PICKUP",
+        "PackagingType": "YOUR_PACKAGING",
+        "TotalWeight": {
+            "Units": "LB",
+            "Value": 1.0
+        },
+        "Shipper": {
+            "Contact": {
+                "CompanyName": "GP"
+            },
+            "Address": {
+                "PostalCode": 98007,
+                "CountryCode": "US"
+            }
+        },
+        "Recipient": {
+            "Address": {
+                "PostalCode": 98007,
+                "CountryCode": "US"
+            }
+        },
+        "ShippingChargesPayment": {
+            "PaymentType": "SENDER",
+            "Payor": {
+                "ResponsibleParty": {
+                    "AccountNumber": credentials['ship_num'] #TODO@@@
+                }
+            }
+        },
+        "RateRequestTypes": "LIST",
+        "PackageCount": 1,
+        "RequestedPackageLineItems": {
+            "SequenceNumber": 1,
+            "GroupNumber": 1,
+            "GroupPackageCount": 1,
+            "Weight": {
+                "Units": "LB",
+                "Value": 1
+            },
+            "Dimensions": {
+                "Length": 1,
+                "Width": 1,
+                "Height": 1,
+                "Units": "IN"
+            }
+        }
+    }
+}
 
-rate = FedexRateServiceRequest(CONFIG_OBJ)
-
-rate.RequestedShipment.DropoffType = 'REGULAR_PICKUP'
-# STANDARD_OVERNIGHT, PRIORITY_OVERNIGHT, FEDEX_GROUND, FEDEX_EXPRESS_SAVER
-rate.RequestedShipment.ServiceType = 'FEDEX_GROUND'
-#  FEDEX_BOX, FEDEX_PAK, FEDEX_TUBE, YOUR_PACKAGING
-rate.RequestedShipment.PackagingType = 'YOUR_PACKAGING'
-
-# Sender info
-rate.RequestedShipment.Shipper.Address.PostalCode = '98007'
-rate.RequestedShipment.Shipper.Address.CountryCode = 'US'
-rate.RequestedShipment.Shipper.Address.Residential = False
-
-# Recipient info
-rate.RequestedShipment.Recipient.Address.StateOrProvinceCode = 'NC'
-rate.RequestedShipment.Recipient.Address.PostalCode = '27577'
-rate.RequestedShipment.Recipient.Address.CountryCode = 'US'
-rate.RequestedShipment.Recipient.Address.Residential = True
-
-rate.RequestedShipment.EdtRequestType = 'NONE'
-rate.RequestedShipment.ShippingChargesPayment.PaymentType = 'SENDER'
-
-package1_weight = rate.create_wsdl_object_of_type('Weight')
-package1_weight.Value = 1.0
-package1_weight.Units = "LB"
-package1 = rate.create_wsdl_object_of_type('RequestedPackageLineItem')
-package1.Weight = package1_weight
-package1.PhysicalPackaging = 'BOX'
-package1.GroupPackageCount = 1
-rate.add_package(package1)
-
-rate.send_request()
-
-
-print(sobject_to_dict(rate.response))
+# print(client.serivce)
+try:
+    # with open('output3.txt', 'w') as f:
+    #     f.write(str(client.service.getRates(**RateRequestDict)))
+    rates = (client.service.getRates(**RateRequestDict))['RateReplyDetails']
+    # with open ('poop.txt', 'w') as f:
+    #     pkg = rates[0]['RatedShipmentDetails']
+    #     f.write(str(list(enumerate(pkg, 0))))
+    
+    for rate in rates:
+        pkg = rate['RatedShipmentDetails'][0]['RatedPackages'][0]['PackageRateDetail']
+        out = rate['ServiceType'] + ' '  + \
+              str(pkg['NetCharge']['Amount']) + ' ' + \
+              str(pkg['BillingWeight']['Value']) + ' ' + \
+              str(pkg['TotalRebates']['Amount'])
+        print(out)
+    
+except Fault as error:
+    print(ET.tostring(error.detail))
